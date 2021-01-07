0b3d5584ceac02509b20be4d6536a3e02c063e27
MatteoTiratelli <69040868+MatteoTiratelli@users.noreply.github.com>
2021-01-07 13:52:51 +0000

    Update Maps.R

--- Maps.R
+++ Maps.R
@@ -3,97 +3,8 @@ library(cowplot)
 library(tmaptools)
 library(sf)
 
-# 5G attacks
-
-PhoneMastAttacks <- read_csv("https://raw.githubusercontent.com/MatteoTiratelli/5GPopulism/main/Annex%20-%20Phone%20mast%20and%20telecoms%20infrastructure%20incident%20data.csv")
-PhoneMastAttacks$MONTH <- substr(PhoneMastAttacks$DATE, 4, 5)
-PhoneMastAttacks$DATE <- as.Date(PhoneMastAttacks$DATE, format = "%d/%m/%Y")
-
-PhoneMastAttacks %>%
-  group_by(MONTH) %>%
-  count() %>%
-  ggplot(data = ., aes(x = as.numeric(MONTH), y = n)) +
-  geom_line() + geom_label(aes(x=4.85, y=100, label="Thomas Cowan viral video released")) +
-  scale_x_continuous(breaks = c(3,5,7,9,11), labels = c("March", "May", "July","September","November")) +
-  scale_y_continuous(limits = c(0,100)) +
-  ylab("Number of attacks on communications infrastructure") + xlab(NULL) +
-  theme_classic() + 
-  geom_vline(xintercept = (3+(12/31)), linetype = 'dashed')
-
-PhoneMastAttacks %>%
-  group_by(COUNTY) %>%
-  count() -> Attacks
-
-Counties <- Attacks$COUNTY
-Counties <- paste(Counties, ", UK", sep="")
-Geocoded <- geocode_OSM(Counties)
-Geocoded$COUNTY <- str_sub(Geocoded$query, 1, -5)
-Geocoded <- Geocoded[,c(2,3,8)]
-
-PhoneMastAttacks <- merge(PhoneMastAttacks, Geocoded, by = "COUNTY")
-Attacks <- merge(Attacks, Geocoded, by = "COUNTY")
-
-
-# Brexit map
-
-AdministrativeCounties <- st_read("https://opendata.arcgis.com/datasets/b216b4c8a4e74f6fb692a1785255d777_0.geojson") # Counties and Unitary Authorities (December 2019) Boundaries UK BUC
-AdministrativeCounties %>%
-  filter(substr(ctyua19cd,1,1) != 'N') -> AdministrativeCounties
-
 EU19 <- read_csv("https://raw.githubusercontent.com/MatteoTiratelli/5GPopulism/main/2019%20EU%20Election%20Results.csv")
-EU19lookup <- read_csv("https://opendata.arcgis.com/datasets/1512e57c5aa34d049569750083765eba_0.csv") # Local Authority District to County (December 2019) Lookup in England
-
-EU19 <- merge(EU19, EU19lookup, by.x = 'la_code', by.y = 'LAD19CD', all = TRUE)
-EU19$CTY19CD <- ifelse(EU19$la_code %in% sf$ctyua19cd, EU19$la_code, EU19$CTY19CD)
 EU19$Brexit <- as.numeric(substr(EU19$Brexit,1,nchar(EU19$Brexit)-1))
-
-EU19 %>%
-  drop_na(CTY19CD) %>%
-  group_by(CTY19CD) %>%
-  summarize(BrexitSupport = (sum((Brexit/100)*`Total votes`)/sum(`Total votes`)),
-            Population = sum(Electorate)) -> BrexitByCounty
-
-merged <- merge(AdministrativeCounties, BrexitByCounty, by.x = "ctyua19cd", by.y='CTY19CD', all = TRUE)
-merged <- st_as_sf(merged)
-
-ggplot() + 
-  geom_sf(data = merged, aes(fill=BrexitSupport)) + 
-  #geom_jitter(data = PhoneMastAttacks[!PhoneMastAttacks$COUNTY %in% c("Antrim", "Armagh"),], 
-  #           aes(x = lon, y = lat), alpha = 0.75, colour = 'black') +
-  theme_void() + coord_sf() +
-  scale_fill_gradient(low = "yellow", high = "purple", limits = c(0, 0.55),
-                      space = "Lab", na.value = "grey50", guide = "colourbar",
-                      aesthetics = "fill", name = "Brexit Party support") +
-  theme(plot.title.position = "plot",
-        plot.caption.position =  "plot",
-        plot.caption = element_text(hjust = 0)) -> Main
-
-LondonCD <- c('E09000007','E09000001','E09000012','E09000013','E09000014','E09000019','E09000020','E09000022',
-              'E09000023','E09000025','E09000028','E09000030','E09000032','E09000033','E09000002','E09000003',
-              'E09000004','E09000005','E09000006','E09000008','E09000009','E09000010','E09000011','E09000015',
-              'E09000016','E09000017','E09000018','E09000021','E09000024','E09000026','E09000027','E09000029',
-              'E09000031')
-
-ggplot() + 
-  geom_sf(data = merged[merged$ctyua19cd %in% LondonCD,], aes(fill=BrexitSupport)) + 
-  #geom_jitter(data = PhoneMastAttacks[!PhoneMastAttacks$COUNTY %in% c("Antrim", "Armagh"),], 
-  #            aes(x = lon, y = lat), alpha = 0.75, colour = 'black') +
-  theme_void() + coord_sf() +
-  scale_fill_gradient(low = "yellow", high = "purple", limits = c(0, 0.55),
-                      space = "Lab", na.value = "grey50", guide = "colourbar",
-                      aesthetics = "fill", name = "Brexit Party support") +
-  ggtitle("London") +
-  theme(legend.position = "none",
-        plot.title.position = "plot",
-        plot.caption.position =  "plot",
-        plot.caption = element_text(hjust = 0)) -> London
-
-ggdraw(Main) +
-  draw_plot(London, width = 0.2, height = 0.2, 
-            x = 0.18, y = 0.26) -> BrexitMap
-
-
-# Regions
 
 Regions <- st_read("https://opendata.arcgis.com/datasets/01fd6b2d7600446d8af768005992f76a_4.geojson")
 Regions$nuts118nm <- c("North East", "North West", "Yorkshire and the Humber", "East Midlands",
@@ -111,15 +22,47 @@ merged <- st_as_sf(merged)
 
 ggplot() + 
   geom_sf(data = merged, aes(fill=BrexitSupport)) + 
-  #geom_jitter(data = PhoneMastAttacks[!PhoneMastAttacks$COUNTY %in% c("Antrim", "Armagh"),], 
-  #           aes(x = lon, y = lat), alpha = 0.75, colour = 'black') +
   theme_void() + coord_sf() +
-  scale_fill_gradient(low = "yellow", high = "purple", limits = c(0, 0.55),
+  scale_fill_gradient(low = "yellow", high = "purple", limits = c(0, 0.4),
                       space = "Lab", na.value = "grey50", guide = "colourbar",
                       aesthetics = "fill", name = "Brexit Party support") +
+  labs(title = "Figure 1: Support for the Brexit Party in the 2019 European Elections by region",
+       caption = "Source: House of Commons elections data") +
   theme(plot.title.position = "plot",
         plot.caption.position =  "plot",
-        plot.caption = element_text(hjust = 0))
+        plot.caption = element_text(hjust = 0),
+        panel.background = element_rect(fill = "transparent", colour = NA),
+        plot.background = element_rect(fill = "transparent", colour = NA),
+        legend.background= element_rect(fill = "transparent", colour = NA))
+
+# 5G attacks
+
+PhoneMastAttacks <- read_csv("https://raw.githubusercontent.com/MatteoTiratelli/5GPopulism/main/Annex%20-%20Phone%20mast%20and%20telecoms%20infrastructure%20incident%20data.csv")
+PhoneMastAttacks$MONTH <- substr(PhoneMastAttacks$DATE, 4, 5)
+PhoneMastAttacks$DATE <- as.Date(PhoneMastAttacks$DATE, format = "%d/%m/%Y")
+
+PhoneMastAttacks %>%
+  group_by(MONTH) %>%
+  count() %>%
+  ggplot(data = ., aes(x = as.numeric(MONTH), y = n)) +
+  geom_line() + 
+  scale_x_continuous(breaks = c(3,5,7,9,11), labels = c("March", "May", "July","September","November")) +
+  scale_y_continuous(limits = c(0,100)) +
+  ylab(NULL) + xlab(NULL) +
+  theme_classic() +
+  labs(title = "Figure 2: The number of attacks on communications infrastructure in 2020", 
+       caption = "Source: Ofcom data released via FOI 1046318") +
+  theme(plot.title.position = "plot",
+        plot.caption.position =  "plot",
+        plot.caption = element_text(hjust = 0),
+        panel.background = element_rect(fill = "transparent", colour = NA),
+        plot.background = element_rect(fill = "transparent", colour = NA),
+        legend.background= element_rect(fill = "transparent", colour = NA))
+
+
+PhoneMastAttacks %>%
+  group_by(COUNTY) %>%
+  count() -> Attacks
 
 Attacks %>%
   mutate(Region = ifelse(COUNTY %in% c("Bedfordshire","Cambridgeshire", "Essex","Hertfordshire"), "East",
@@ -135,6 +78,7 @@ Attacks %>%
                                                                                         ifelse(COUNTY %in% c("South Yorkshire","West Yorkshire"), "Yorkshire and the Humber",
                                                                                                ifelse(COUNTY %in% c("Tyne & Wear", "Tyne and Wear"), "North East",
                                                                                                       ifelse(COUNTY %in% c("Warwickshire","West Midlands"), "West Midlands", NA)))))))))))))) -> Attacks
+
 Attacks %>%
   drop_na(Region) %>%
   group_by(Region) %>%
@@ -145,30 +89,36 @@ merged <- st_as_sf(merged)
 
 ggplot() + 
   geom_sf(data = merged, aes(fill=n)) + 
-  #geom_jitter(data = PhoneMastAttacks[!PhoneMastAttacks$COUNTY %in% c("Antrim", "Armagh"),], 
-  #           aes(x = lon, y = lat), alpha = 0.75, colour = 'black') +
   theme_void() + coord_sf() +
   scale_fill_gradient(low = "yellow", high = "purple",
                       space = "Lab", na.value = "grey50", guide = "colourbar",
                       aesthetics = "fill", name = "Phone mast attacks") +
+  labs(title = "Figure 3: Attacks on communications infrastructure in 2020 by region", 
+       caption = "Source: Ofcom data released via FOI 1046318") +
   theme(plot.title.position = "plot",
         plot.caption.position =  "plot",
-        plot.caption = element_text(hjust = 0))
+        plot.caption = element_text(hjust = 0),
+        panel.background = element_rect(fill = "transparent", colour = NA),
+        plot.background = element_rect(fill = "transparent", colour = NA),
+        legend.background= element_rect(fill = "transparent", colour = NA))
 
-# Not working... Probably need a manual lookup table or some way of summarising
+EU19 %>% #Merseyside
+  filter(la_code %in% c("E08000011","E08000012","E08000013","E08000014","E08000015")) %>%
+  summarize(BrexitSupport = (sum((Brexit/100)*`Total votes`)/sum(`Total votes`)),
+            Population = sum(Electorate))
 
-CeremonialCounties <- st_read("https://opendata.arcgis.com/datasets/5b60fac33976436ab900e05eb1a33216_12.csv") # Boundary-Line ceremonial counties NOT WORKING
+EU19 %>% #West Yorkshire
+  filter(la_code %in% c("E08000032","E08000033","E08000034","E08000035","E08000036")) %>%
+  summarize(BrexitSupport = (sum((Brexit/100)*`Total votes`)/sum(`Total votes`)),
+            Population = sum(Electorate))
 
-merged <- as.data.frame(merge(CeremonialCounties, Attacks, by.x = "ctyua19cd", by.y='COUNTY', all = TRUE))
-merged <- st_as_sf(merged)
+EU19 %>% #West Midlands
+  filter(la_code %in% c("E08000025","E08000026","E08000027","E08000028","E08000029","E08000030","E08000031")) %>%
+  summarize(BrexitSupport = (sum((Brexit/100)*`Total votes`)/sum(`Total votes`)),
+            Population = sum(Electorate))
 
-ggplot() + 
-  geom_sf(data = merged, aes(fill=BrexitSupport)) + 
-  theme_void() + coord_sf() +
-  scale_fill_gradient(low = "yellow", high = "purple", limits = c(0, 0.55),
-                      space = "Lab", na.value = "grey50", guide = "colourbar",
-                      aesthetics = "fill", name = "Brexit Party support") +
-  theme(plot.title.position = "plot",
-        plot.caption.position =  "plot",
-        plot.caption = element_text(hjust = 0)) -> Main
-
+EU19 %>%
+  mutate(Nation = substr(la_code,1,1)) %>%
+  group_by(Nation) %>%
+  summarize(BrexitSupport = (sum((Brexit/100)*`Total votes`)/sum(`Total votes`)),
+            Population = sum(Electorate))
